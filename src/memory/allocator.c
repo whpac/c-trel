1a31f4a7d2f0f5e8a0a76ca59b76337fd6655fa3
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-11-06 19:32:03 +0100

    It doesn't log every allocation now

--- src/memory/allocator.c
+++ src/memory/allocator.c
@@ -4,11 +4,11 @@
 
 void* getMemory(unsigned int size){
     void* ptr = malloc(size);
-    printf("[Alloc]: %u bytes at addr %llu\n", size, (unsigned long long)ptr);
+    // printf("[Alloc]: %u bytes at addr %llu\n", size, (unsigned long long)ptr);
     return ptr;
 }
 
 void freeMemory(void* mem){
-    printf("[Dealloc]: at addr %llu\n", (unsigned long long)mem);
+    // printf("[Dealloc]: at addr %llu\n", (unsigned long long)mem);
     free(mem);
 }
